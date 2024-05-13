1f5921f6c3bb5052c42e574954cd12cf3d04fdad
osmanli azer <azer@osmanli.com>
2024-05-14 00:19:24 +0400

    new

---
+++ Controllers/ShopController1.cs
@@ -0,0 +1,17 @@
+﻿
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationPronia.Controllers
+{
+    public class ShopController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> AboutUs()
+        {
+            return View();
+        }
+    }
+}
---
+++ DataAccesLayer/ProniaContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pronia.Models;
+
+namespace Pronia.DataAccesLayer
+{
+    public class ProniaContext : DbContext
+    {
+        public ProniaContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Slider> Sliders { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+        public DbSet<Product> Products { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Server=sql8010.site4now.net;Database=db_aa860b_azer;User Id=db_aa860b_azer_admin;Password=whcy1583;");
+            base.OnConfiguring(options);
+        }
+    }
+}
